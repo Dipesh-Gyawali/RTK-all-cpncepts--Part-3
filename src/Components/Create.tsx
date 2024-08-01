fe4dafb1a13bfd6e58ea2e52867412ad286291b9
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-08-01 14:03:45 +0545

    feat: create API hit

--- src/Components/Create.tsx
+++ src/Components/Create.tsx
@@ -1,23 +1,64 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createUser } from "../features/userDetailSlice";
 
 export const Create = () => {
+  const [userData, setUserData] = useState({});
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userData, "userData");
+    dispatch(createUser(userData));
+  };
+
   return (
     <div>
       <h2>Enter the data</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input type="text" name="name" placeholder="enter name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="enter name"
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <input type="email" name="email" placeholder="enter email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="enter email"
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <input type="number" name="age" placeholder="enter age" />
+          <input
+            type="number"
+            name="age"
+            placeholder="enter age"
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <input type="radio" name="gender" value="Male" />
+          <input
+            type="radio"
+            name="gender"
+            value="Male"
+            onChange={handleChange}
+          />
           <label>Male</label>
-          <input type="radio" name="gender" value="Female" />
+          <input
+            type="radio"
+            name="gender"
+            value="Female"
+            onChange={handleChange}
+          />
           <label>Female</label>
         </div>
         <div>
